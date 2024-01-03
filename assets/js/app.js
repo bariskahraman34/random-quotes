d685fdd2abab7f9fe56eb48f5628b700b3b79b9b
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-04 00:46:29 +0300

    Random quote lar js tarafında oluşturuldu. Kullanıcı her ileri butonuna tıkladığında random bir quote card container ın içine eklenmesi işlemi yapıldı. Lorempicsum ile body'e her bir quote için random bir fotoğraf atandı

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,24 @@
-const quoteSubject = document.querySelector('#quoteSubject')
+const nextQuoteBtn = document.querySelector('.next-quote-btn');
+const tag = document.querySelector('.tags');
+const quoteContent = document.querySelector('.quote-content');
+const cardAuthor = document.querySelector('.card-author');
+const body = document.querySelector('body');
 
 async function fetchQuotesSubjects(){
     const response = await fetch("https://api.quotable.io/random");
     const data = await response.json();
     return data
 }
+let counter = 0;
+
+body.style.background = `url(https://picsum.photos/${window.innerWidth}/${window.innerHeight}?random=${counter})`;
+
+nextQuoteBtn.addEventListener('click',async function(){
+    const data = await fetchQuotesSubjects();
+    tag.innerText = `${data.tags}`;
+    quoteContent.innerText = `${data.content}`;
+    cardAuthor.innerText = `${data.author}`;
+    counter ++;
+    body.style.background = `url(https://picsum.photos/${window.innerWidth}/${window.innerHeight}?random=${counter})`;
+
+})
