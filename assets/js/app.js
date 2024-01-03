b5c007704420c4d2400715046c1031d87b2b0d51
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-04 00:18:20 +0300

    Quote için css çalışmaları tamamlandı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,5 @@ const quoteSubject = document.querySelector('#quoteSubject')
 async function fetchQuotesSubjects(){
     const response = await fetch("https://api.quotable.io/random");
     const data = await response.json();
-    console.log(data)
     return data
 }
-
-fetchQuotesSubjects();
