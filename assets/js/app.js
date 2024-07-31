e6aaec9068cd4e4460e19b43762949fbca221605
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-31 23:24:18 +0300

    Yeni API eklendi. Random id üreteci eklendi ve aynı idlerin gelmemesi için önceki sayıları kontrol eden bir fonksiyon yazıldı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,30 @@ const quoteContent = document.querySelector('.quote-content');
 const cardAuthor = document.querySelector('.card-author');
 const body = document.querySelector('body');
 
+const numbersArray = []
+
+function checkPreviousRandomNumbers(number,initialValue,endValue){
+    if(numbersArray.length > 0){
+        const searchedNumber = numbersArray.find((num) => num === number);
+        if(searchedNumber){
+            return randomIdGenerator(initialValue,endValue)
+        }else{
+            numbersArray.push(number)
+            return number
+        }
+    }else{
+        numbersArray.push(number)
+        return number
+    }
+}
+
+function randomIdGenerator(initialValue, endValue){
+    const randomNumber = Math.floor(initialValue + Math.random() * endValue)
+    return checkPreviousRandomNumbers(randomNumber,initialValue,endValue)
+}
+
 async function fetchQuotesSubjects(){
-    const response = await fetch("https://api.quotable.io/random");
+    const response = await fetch(`https://dummyjson.com/quotes/${randomIdGenerator(1,1453)}`);
     const data = await response.json();
     return data
 }
@@ -18,8 +40,7 @@ body.style.backgroundSize = "cover";
 
 nextQuoteBtn.addEventListener('click',async function(){
     const data = await fetchQuotesSubjects();
-    tag.innerText = `${data.tags}`;
-    quoteContent.innerText = `${data.content}`;
+    quoteContent.innerText = `${data.quote}`;
     cardAuthor.innerText = `${data.author}`;
     counter ++;
     body.style.background = `url(https://picsum.photos/${window.innerWidth}/${window.innerHeight}?random=${counter}.webp)`;
